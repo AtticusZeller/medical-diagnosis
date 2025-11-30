95737562fa741d4dd2a9e3cf1c489fe497b8199e
Atticuszz <1831768457@qq.com>
2025-11-30 16:59:38 +0800

    Remove unused code blocks

--- src/expt/utils.py
+++ src/expt/utils.py
@@ -6,9 +6,6 @@ from rich import print
 from timm.data.config import resolve_data_config
 from timm.data.transforms_factory import create_transform
 from torch import nn
-from torch.utils.data import DataLoader
-
-from expt.data.dataset import DataModule
 
 
 def create_rich_progress_bar() -> RichProgressBar:
@@ -28,16 +25,6 @@ def create_rich_progress_bar() -> RichProgressBar:
             metrics_format=".3e",
         )
     )
-
-
-def mean_std(data_module: DataModule) -> tuple[float, float]:
-    dataset = data_module.dataset_class(
-        data_module.data_dir, train=True, transform=data_module.transform
-    )
-    loader: DataLoader = DataLoader(dataset, batch_size=len(dataset), pin_memory=True)
-
-    data = next(iter(loader))[0]
-    return data.mean().item(), data.std().item()
 
 
 def check_transform(model: nn.Module) -> None:
